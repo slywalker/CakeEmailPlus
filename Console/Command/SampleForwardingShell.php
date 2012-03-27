96596e1f95cb6e5d688f80a871f1d2822e30287d
Yasuo Harada <slywalker.net@gmail.com>
2012-03-27 11:14:09 +0900

    add sample

---
+++ Console/Command/SampleForwardingShell.php
@@ -0,0 +1,34 @@
+<?php
+App::uses('AppShell', 'Console/Command');
+App::uses('ImapEmail', 'EmailPlus.Network/Email');
+App::uses('ForwardEmail', 'EmailPlus.Network/Email');
+
+class SampleForwardingShell extends AppShell {
+
+	public $to = 'someone@example.com';
+
+	public function main() {
+		$email = new ForwardEmail;
+
+		$imap = new ImapEmail('forwardImap');
+
+		$results = $imap->search('UNSEEN');
+
+		foreach ($results as $mid) {
+			$overview = $imap->fetchOverview($mid);
+
+			$header = $imap->fetchHeader($mid);
+
+			$body = $imap->body($mid);
+
+			$res = $email->reset()
+				->config('forwardSmtp')
+				->originalHeader($header)
+				->originalBody($body)
+				->from($overview['to'])
+				->to($this->to)
+				->send();
+		}
+	}
+
+}
